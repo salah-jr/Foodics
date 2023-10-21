b8ba6e01bed9348bbb8b6752f5564bf800a482b0
salah-jr <mohammed.s.korna@gmail.com>
2023-10-21 17:01:01 +0200

    Managing the purshase flow

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\IngredientStockHistory;
 use App\Models\Order;
 use App\Models\Product;
+use Exception;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\OrderRequest;
 use App\Http\Controllers\Controller;
@@ -13,20 +15,59 @@ class OrderController extends Controller
     public function placeOrder(OrderRequest $request)
     {
         $products = $request->get('products');
+        $order = Order::create();
 
-        //        DB::beginTransaction();
+        try {
 
-        //        $order = Order::create();
+            DB::beginTransaction();
+            foreach ($products as $productDetails) {
 
-        foreach ($products as $productDetails) {
+                $product = Product::with('ingredients')->findOrFail($productDetails['product_id']);
+                $quantity = $productDetails['quantity'];
+                $order->products()->attach($product->id, ['quantity' => $quantity]);
 
-            $product = Product::findOrFail($productDetails['product_id']);
-            $quantity = $productDetails['quantity'];
+                foreach ($product->ingredients as $ingredient) {
+                    $totalUsedQuantityInGrams = $ingredient->pivot->quantity * $quantity;
 
-            foreach ($product->ingredients as $ingredient) {
-               $totalUsedQuantityInGrams = $ingredient->pivot->quantity * $quantity;
-               $totalQuantityInKilograms = $totalUsedQuantityInGrams / 1000;
+                    $totalQuantityInKilograms = $totalUsedQuantityInGrams / 1000;
+
+                    if ($ingredient->available_stock < $totalQuantityInKilograms) {
+                        return response()->json(['error' => 'Insufficient stock for ' . $ingredient->name], 400);
+                    }
+
+                    $newAvailableStock = $ingredient->available_stock - $totalQuantityInKilograms;
+                    $emailSent = false;
+
+                    if ($ingredient->available_stock < $ingredient->stock * 0.5) {
+                        $emailSent = true;
+                    }
+
+                    if (!$emailSent) {
+                        // Todo:: send email notification to the merchant
+                    }
+
+                    $ingredient->update([
+                        'available_stock' => $newAvailableStock
+                    ]);
+                }
             }
+
+            $order->update([
+               'status' => Order::$success,
+            ]);
+
+            DB::commit();
+
+            return response()->json(['message' => 'Order processed successfully'], 200);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            $order->update([
+                'status' => Order::$failed,
+            ]);
+
+            return response()->json(['error' => 'An error occurred while processing the order'], 500);
         }
+
     }
 }
