1af5d8ce1f396ccb9fb88bf254ece79399d986e1
salah-jr <mohammed.s.korna@gmail.com>
2023-10-21 14:37:51 +0200

    Create Order Request

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'products' => 'required|array',
+            'products.*.product_id' => 'required|exists:products,id',
+            'products.*.quantity' => 'required|integer|min:1',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'products.*.product_id' => 'product',
+            'products.*.quantity' => 'quantity',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+             'message' => implode('', collect($validator->errors())->first()),
+             'status' => true
+        ], 422));
+    }
+}
