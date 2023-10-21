909670ce1a57a0d2e3465bb6ffab393918a5fa68
salah-jr <mohammed.s.korna@gmail.com>
2023-10-21 14:36:56 +0200

    Create ingredient stock history model

---
+++ app/Models/IngredientStockHistory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IngredientStockHistory extends Model
+{
+    use HasFactory;
+
+    public function ingredient()
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
+}
