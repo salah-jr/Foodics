f1f286443aec65c9c7eded3184bbca52dd4bc6e1
salah-jr <mohammed.s.korna@gmail.com>
2023-10-22 00:31:25 +0200

    Add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory()->create();
+        $user->createToken('access_token')->plainTextToken;
+       dd( $user->currentAccessToken());
+    }
+}
