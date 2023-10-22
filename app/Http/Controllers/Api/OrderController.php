740cd08546e2efd43e94e2f7399aed05d5e17f33
salah-jr <mohammed.s.korna@gmail.com>
2023-10-22 21:59:38 +0200

    Add import for JsonResponse

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Services\OrderService;
 use App\Http\Requests\OrderRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 
 class OrderController extends Controller
 {
-    public function placeOrder(OrderRequest $request, OrderService $orderService): \Illuminate\Http\JsonResponse
+    public function placeOrder(OrderRequest $request, OrderService $orderService): JsonResponse
     {
         return $orderService->handle($request);
     }
